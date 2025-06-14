6a3e8b3c733eef462c63eb8c59caa63203682098
rushikesh982 <garadrushikesh042@gmail.com>
2025-06-14 18:18:24 +0530

    creating navbar and features

--- src/components/Navbar/Header.jsx
+++ src/components/Navbar/Header.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css';
 import Logo from '../Assets/footerLogo.webp'
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
+    let [changeColor,setChangeColor] = useState();
   return (
     <>
         <div className="navOuter">
@@ -12,11 +13,25 @@ const Header = () => {
             </div>
             <div className="navItem">
                 <ul>
-                    <li><NavLink className={'nav-link'} to={'/'}>SHOP</NavLink></li>
-                    <li><NavLink className={'nav-link'} to={'/workshop'}>WORKSHOP</NavLink></li>
-                    <li><NavLink className={'nav-link'} to={'/interiorProjects'}>INTERIORS PROJECT</NavLink></li>
-                    <li><NavLink className={'nav-link'} to={'/contact'}>CONTACT</NavLink></li>
-                    <li><NavLink className={'nav-link'} to={'/ourStory'}>OUR STORY</NavLink></li>
+                    <li>
+                        <NavLink onClick={
+                            ()=>{setChangeColor('shop')}
+                            } className={
+                                ` ${changeColor == 'shop'?"activeColor":'dactiveColor'}`
+                                } to={'/'}>SHOP
+
+                                </NavLink>
+                                </li>
+                    <li>
+                        <NavLink onClick={
+                            ()=>{setChangeColor('workshop')}
+                            } className={
+                                ` ${changeColor == 'workshop'?'activeColor':'dactiveColor'}`
+                                } to={'/workshop'}>WORKSHOP</NavLink>
+                                </li>
+                    <li><NavLink onClick={()=>{setChangeColor('interiorPorject')}} className={` ${changeColor == 'interiorPorject'?'activeColor':"dactiveColor"}`} to={'/interiorProjects'}>INTERIORS PROJECT</NavLink></li>
+                    <li><NavLink onClick={()=>{setChangeColor('contact')}} className={`${changeColor == 'contact'?'activeColor':'dactiveColor'}`} to={'/contact'}>CONTACT</NavLink></li>
+                    <li><NavLink onClick={()=>{setChangeColor('ourStory')}} className={changeColor == 'ourStory'?'activeColor':'dactiveColor'} to={'/ourStory'}>OUR STORY</NavLink></li>
                 </ul>
             </div>
             <div className="navRight">
@@ -30,7 +45,7 @@ const Header = () => {
                     <li>
                         <i class="ri-shopping-cart-2-line"></i>
                     </li>
-                    <li><i class="ri-menu-line"></i></li>
+                    <li><i class="ri-menu-line menu"></i></li>
                 </ul>
             </div>
         </div>
