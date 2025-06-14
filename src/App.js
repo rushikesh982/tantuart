531286b2d5fccf4ac840595143592ba91c0488e0
rushikesh982 <garadrushikesh042@gmail.com>
2025-06-14 11:58:03 +0530

    change component name

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './components/Navbar/Header';
-import Footer from './components/Footer/Footer.jsx';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
