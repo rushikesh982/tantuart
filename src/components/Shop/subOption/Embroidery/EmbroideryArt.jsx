e50b08f83d448cd9a72a6ecdb2312928cb6f4c6e
rushikesh982 <garadrushikesh042@gmail.com>
2025-06-25 20:39:40 +0530

    adding shop some sub options and creating their components

---
+++ src/components/Shop/subOption/Embroidery/EmbroideryArt.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './EmbroideryArt.css'
+
+export default function EmbroideryArt()
+{
+    return(
+        <>
+            <h1>EmbroideryArt</h1>
+        </>
+    )
+}
