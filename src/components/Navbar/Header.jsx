c55118c2a8e5b2e7cb61a03a154604715d754398
rushikesh982 <garadrushikesh042@gmail.com>
2025-06-26 10:55:10 +0530

    updating feature

--- src/components/Navbar/Header.jsx
+++ src/components/Navbar/Header.jsx
@@ -54,7 +54,7 @@ const Header = () => {
                     setActiveTab("shop");
                   }}>
                 <NavLink
-                  to="/shop"
+                  // to="/shop"
   
                   
                 >
